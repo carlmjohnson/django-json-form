4f2fb6e27ded46f44dd915734baddbe9d5edbeb3
Carl Johnson <cmjohnson@pbs.org>
2016-04-29 11:58:26 +0300

    Example: Allow multiple forms on one page

--- jsonform_example/static/jsonform_example/js/ajax-loader.js
+++ jsonform_example/static/jsonform_example/js/ajax-loader.js
@@ -1,35 +1,39 @@
 $(document).ready(function() {
-    var $container = $("#json-form-container");
+    var bindContainer = (function($container){
+        return (function(data) {
+            // So we can manipulate the last loaded data from the console
+            window.data = data;
 
-    var success = (function(data) {
-        // So we can manipulate the last loaded data from the console
-        window.data = data;
+            $container.html(data.html);
 
-        $container.html(data.html);
+            $container.find("form[ajax=true]").submit(function(e) {
 
-        $container.find("form[ajax=true]").submit(function(e) {
+                e.preventDefault();
+                var $this = $(this);
 
-            e.preventDefault();
-            var $this = $(this);
+                var form_data = $this.serialize();
+                var form_url = $this.attr("action");
+                var form_method = $this.attr("method").toUpperCase();
 
-            var form_data = $this.serialize();
-            var form_url = $this.attr("action");
-            var form_method = $this.attr("method").toUpperCase();
-
-            $.ajax({
-                url: form_url,
-                dataType: "json",
-                type: form_method,
-                data: form_data,
-                cache: false,
-                success: success
+                $.ajax({
+                    url: form_url,
+                    dataType: "json",
+                    type: form_method,
+                    data: form_data,
+                    cache: false,
+                    success: bindContainer($this)
+                });
             });
         });
     });
 
-    $.ajax({
-        dataType: "json",
-        url: $container.data("ajax-url"),
-        success: success
+    $("[data-ajax-url]").each(function(){
+        var $el = $(this);
+
+        $.ajax({
+            dataType: "json",
+            url: $el.data("ajax-url"),
+            success: bindContainer($el)
+        });
     });
 });
